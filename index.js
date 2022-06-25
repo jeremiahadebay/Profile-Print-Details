a8e09dbb4c046c75ebafd5605639beabc7fe3c10
jeremiahadebay <bamsonj@gmail.com>
2022-06-25 13:54:40 +0100

    Profile details added

---
+++ index.js
@@ -0,0 +1,5 @@
+let myName = ("Jeremiah Adebayo");
+let myHeight = ("about 5.7ft");
+let myCountry = ("Nigeria");
+let myProfile = ("My name is " + myName + ". My height is " + myHeight + ". I am from " + myCountry + ".")
+alert(myProfile)
